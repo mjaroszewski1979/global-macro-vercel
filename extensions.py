f7fd6480f235dfa05c03875b080618a971350202
mjaroszewski <mjaroszewski1979@gmail.com>
2023-04-19 19:26:07 +0200

    added test deployment project

--- extensions.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy 
-
-# Data base instantiation
-db = SQLAlchemy()
--- settings.py
+++
@@ -1,5 +0,0 @@
-from os import environ
-
-SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
