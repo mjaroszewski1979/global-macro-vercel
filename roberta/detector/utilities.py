ec1ba796ed56ca9f3d52d8e221e8547f11c1e176
mjaroszewski <mjaroszewski1979@gmail.com>
2023-04-26 20:52:03 +0200

    added base template and fixed get_result function

--- roberta/detector/utilities.py
+++ roberta/detector/utilities.py
@@ -1,9 +1,10 @@
 import requests
+import time
 
 # This function queries an API to get a result based on the input data
 def get_result(data):
     # Set API token and URL
-    API_TOKEN = ''
+    API_TOKEN = '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'
     API_URL = "https://api-inference.huggingface.co/models/roberta-large-openai-detector"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
@@ -22,13 +23,17 @@ def get_result(data):
 # This function takes a result as input and returns a boolean score based on the label
 def get_score(result):
     # Iterate through the first element in the result list
-    for element in result[0]:
-        # Check if the label is 'LABEL_0'
-        if element['label'] == 'LABEL_0':
-            label_0 = element['score']
-             # If the score for 'LABEL_0' is greater than 0.5, return False
-            if label_0 > 0.5:
-                return False
-            # Otherwise, return True
-            else:
-                return True
+    if type(result) == dict:
+        time.sleep(20)
+
+    else:
+        for element in result[0]:
+            # Check if the label is 'LABEL_0'
+            if element['label'] == 'LABEL_0':
+                label_0 = element['score']
+                # If the score for 'LABEL_0' is greater than 0.5, return False
+                if label_0 > 0.5:
+                    return False
+                # Otherwise, return True
+                else:
+                    return True
