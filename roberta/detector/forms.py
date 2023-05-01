44f91699f98107032635b6b1fcb5aacdedaf4551
mjaroszewski <mjaroszewski1979@gmail.com>
2023-05-01 17:57:24 +0200

    fixe folder settings

--- roberta/detector/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class ContentForm(forms.Form):
-    content = forms.CharField(widget=forms.Textarea(attrs={"rows":15, "cols":200}), max_length=1000, help_text="1000 characters max.")
