20ecfe07a493a8201889dd8f333d4fe7f36e1b7b
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-07 20:09:05 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
+# Print a message indicating the build process is starting
 echo " BUILD START"
+
+# Install the required Python packages specified in the requirements.txt file using pip
+# The -m option runs the pip module as a script using Python 3.9
 python3.9 -m pip install -r requirements.txt
+
+# Collect all the static files in the Django project
+# The --noinput option runs the command without prompting for user input
+# The --clear option deletes the existing static files before collecting the new ones
 python3.9 manage.py collectstatic --noinput --clear
+
+# Print a message indicating the build process has ended
 echo " BUILD END" 
